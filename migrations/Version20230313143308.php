c428a794e6d30dc70aab7433830cc73280eef66d
Aliseth <charlotte.saidi@outlook.fr>
2023-03-13 15:33:38 +0100

    ajoute adresse mac sur user

---
+++ migrations/Version20230313143308.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230313143308 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD mac_address VARCHAR(255) NOT NULL, ADD email VARCHAR(255) NOT NULL, ADD decrypt_id VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `user` DROP mac_address, DROP email, DROP decrypt_id');
+    }
+}
